a59ffb3f30068ca65b3110177415f3f2ab4b5fc0
El3wady1 <niroelawady@gmail.com>
2024-01-27 00:55:55 +0200

    signuplocal

---
+++ test4/lib/endpoints.dart
@@ -0,0 +1,17 @@
+class Endpoints {
+static  String Urlbaseregister = "https://reqres.in/api/register";
+static  String login = "api/login";
+}
+
+// login
+// {
+// "email": "eve.holt@reqres.in",
+// "password": "cityslicka"
+// }
+
+//regist
+
+// {
+// "email": "eve.holt@reqres.in",
+// "password": "pistol"
+// }
