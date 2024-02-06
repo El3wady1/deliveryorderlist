dc762794d1a756f5c29e51ceeabb86b96b09a497
El3wady1 <niroelawady@gmail.com>
2024-02-06 17:57:29 +0200

    OnboardingScreen

---
+++ onbording/lib/views/onboardingview.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:onbording/views/widgets/onboardingbody.dart';
+
+class OnboardingView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return OnboardingBodyView();
+  }
+}
